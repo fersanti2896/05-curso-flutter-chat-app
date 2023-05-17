9243db64807b9e41c3aed3d86546951fd56bf64f
Fernando Nicolás <fersanti2896@gmail.com>
2023-05-17 17:37:01 -0600

    ListView en chat_screen.dart

--- lib/presentation/chat/chat_screen.dart
+++ lib/presentation/chat/chat_screen.dart
@@ -7,14 +7,38 @@ class ChatScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Padding(
-          padding: EdgeInsets.all(8.0),
-          child: CircleAvatar(
-            backgroundImage: NetworkImage('https://m.media-amazon.com/images/I/41qPvq9NJML._CR0,121,455,455_UX256.jpg'),
+          leading: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://m.media-amazon.com/images/I/41qPvq9NJML._CR0,121,455,455_UX256.jpg'),
+            ),
           ),
+          centerTitle: true,
+          title: const Text('Mi amor')),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      left: false,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+            const Text('Mundo')
+          ],
         ),
-        centerTitle: true,
-        title: const Text('Mi amor')
       ),
     );
   }
