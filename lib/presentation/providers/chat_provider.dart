a53f8b1beb11ff570402baee577cd9fe061145b4
Fernando Nicolás <fersanti2896@gmail.com>
2023-07-09 22:23:38 -0600

    Comunicación con API y manejo de mappers

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messagesList = [
     Message(text: 'Hola mi amor!', fromWho: FromWho.me),
@@ -15,6 +17,10 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
     messagesList.add(newMessage);
 
+    if (text.endsWith('?')) {
+      heyReply();
+    }
+
     notifyListeners();
     moveScrollToBottom();
   }
@@ -26,5 +32,13 @@ class ChatProvider extends ChangeNotifier {
         chatScrollController.position.maxScrollExtent,
         duration: const Duration(milliseconds: 300),
         curve: Curves.easeOut);
+  }
+
+  Future<void> heyReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messagesList.add(herMessage);
+
+    notifyListeners();
+    moveScrollToBottom();
   }
 }
